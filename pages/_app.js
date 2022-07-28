dc0ceef47b348fcce3039747131ad7b50431243e
Rian Sanjaya <rian.sj@gmail.com>
2022-07-28 22:57:06 +0700

    use google font, add loading progress indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Layout from '../components/layout/Layout';
+import 'nprogress/nprogress.css'; //styles of nprogress
 import '../styles/globals.scss'
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
